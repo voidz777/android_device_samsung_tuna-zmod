4824181203d02a9bc8e1f77eb452e4044f75a2a0
voidz777 <voidz.evo3d@gmail.com>
2015-11-28 08:08:23 +0900

    Enable tethering

--- device.mk
+++ device.mk
@@ -139,6 +139,10 @@ PRODUCT_PACKAGES += \
 	Nfc \
 	Tag
 
+# Allow tethering without provisioning app
+PRODUCT_PROPERTY_OVERRIDES += \
+    net.tethering.noprovisioning=true
+
 # Live Wallpapers
 PRODUCT_PACKAGES += \
 	LiveWallpapers \
